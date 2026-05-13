30d667bc2035a423047e9a681200faeeec020e77
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-05-12 20:32:45 -0700

    Update mem.v

--- MIPS Pipeline/sources_1/Stage 4 - Memory and Write-back/mem.v
+++ MIPS Pipeline/sources_1/Stage 4 - Memory and Write-back/mem.v
@@ -1,25 +1,21 @@
-`timescale 1ns / 1ps
-
 module data_memory(
     input wire [31:0] addr,
     input wire [31:0] write_data,
     input wire memread, memwrite, clk,
-    output reg [31:0] read_data
+    output wire [31:0] read_data      // <-- wire, not reg
 );
-
     reg [31:0] DMEM [0:255];
     integer i;
 
     initial begin
-        read_data = 0;
         for (i = 0; i < 256; i = i + 1)
             DMEM[i] = 32'h00000000;
         $readmemb("data.mem", DMEM);
     end
 
+    assign read_data = memread ? DMEM[addr] : 32'h00000000;
+
     always @(posedge clk) begin
-        if (memread)
-            read_data <= DMEM[addr];
         if (memwrite)
             DMEM[addr] <= write_data;
     end
